16b7b07a4c29a14a35a7bf5d66f6555e4ff3c147
david31 <david31st@gmail.com>
2023-09-27 18:04:33 +0200

    playerstate class ok

--- player.js
+++ player.js
@@ -1,3 +1,5 @@
+import { Sitting, Running, Jumping, Falling } from './playerStates.js';
+
 export class Player {
     constructor(game) {
         this.game = game;
@@ -6,15 +8,24 @@ export class Player {
         this.x = 0;
         this.y = this.game.height - this.height;
         this.vy = 0;         // vertical speed
-        this.maxVy = 28;     // max vertical speed
-        this.weight = 1;     // vertical weight gravity
+        this.maxVy = 20;     // max vertical speed
+        this.weight = 0.5;     // vertical weight gravity
         this.image = document.getElementById('player');
-        this.frame = 0;      // horizontal frame of picture
+        this.frameX = 0;      // horizontal frame of picture
+        // this.maxFrame = 4     // innit max frame to sitting line (x) of picture (not necessary) 
+        this.fps = 60;
+        this.frameInterval = 1000 / this.fps;
+        this.frameTimer = 0;
+        this.frameY = 0;      // vertical frame of picture
         this.speed = 0;      // horizontal speed
         this.maxSpeed = 10;  // horizontal max speed
+        this.states = [new Sitting(this), new Running(this), new Jumping(this), new Falling(this)];
+        this.currentState = this.states[0];  //  innit state to sitting
+        this.currentState.enter();
     }
 
-    update(input) {
+    update(input, deltaTime) {
+        this.currentState.handleInput(input);
         // Horizontal movement ------------------
         this.x += this.speed;
         if (input.includes('ArrowRight')) this.speed = this.maxSpeed;
@@ -24,25 +35,36 @@ export class Player {
         if (this.x > this.game.width - this.width) this.x = this.game.width - this.width;
         else if (this.x <= 0) this.x = 0;
         // Vertical movement ---------------------
-        if (input.includes('ArrowUp') && this.onGround()) this.vy -= this.maxVy;
         this.y += this.vy;
         if (!this.onGround()) this.vy += this.weight;
         else this.vy = 0;
+        // Sprite animations ----------------------
+        if (this.frameTimer > this.frameInterval) {
+            this.frameTimer = 0;
+            if (this.frameX < this.maxFrame) this.frameX++;
+            else this.frameX = 0;
+        } else {
+            this.frameTimer += deltaTime;
+        }
     }
 
     draw(context) {
         context.drawImage(this.image,
-            this.frame * this.width,
-            0,
-            this.width,
-            this.height,
-            this.x,
-            this.y,
-            this.width,
-            this.height);
+            this.frameX * this.width,    // source(image) x
+            this.frameY * this.height,   // source(image) y
+            this.width,                  // source(image) width
+            this.height,                 // source(image) height
+            this.x,                      // canvas position x
+            this.y,                      // canvas position y
+            this.width,                  // canvas width
+            this.height);                // canvas height
     }
     // return true if the player is on ground and false if the player is in the air
     onGround() {
         return this.y >= this.game.height - this.height;
+    }
+    setState(state) {
+        this.currentState = this.states[state];
+        this.currentState.enter();
     }
 }
